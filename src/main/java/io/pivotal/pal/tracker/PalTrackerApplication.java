367594af470ffbb3be6c73e8b8ea2dfd3a0a1a99
venkatesh.mohanan@cognizant.com <venkatesh.mohanan@cognizant.com>
2020-03-01 22:19:12 +0530

    Spring JDBC template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
